b5f5e4ee44f05e8a23400cafe2f961812a379c9b
coport-uni <ohsungwoo@unist.ac.kr>
2026-04-15 02:13:37 +0000

    bash created

--- hkvisionapi_example.py
+++ hkvisionapi_example.py
@@ -2,7 +2,7 @@ from hikvisionapi import Client
 import cv2
 import time
 
-cam = Client('http://192.168.1.218:218', 'admin', 'peal2024', timeout=30)
+cam = Client('http://192.168.1.218:218', 'admin', password , timeout=30)
 response = cam.Streaming.channels[102].picture(method='get', type='opaque_data'
 )
 
